162530876ec8bd7c4d45c017dcd9e6af8f1f1307
Ikeh Leonard <leoikeh99@gmail.com>
2022-04-30 13:42:16 +0100

    Reinstalled and setup tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./css/tailwind.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Main } from "./pages/Main";
 
 function App() {
   return (
-    <div className="App">
-      <p>hello world</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
