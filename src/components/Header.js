02fd3acac2b088ed4936f87970fe4a277ea4f7df
Ikeh Leonard <leoikeh99@gmail.com>
2022-04-30 16:20:35 +0100

    Setup Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,38 @@
 import React from "react";
+import MenuIcon from "../assets/icon-menu.svg";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+import { ReactComponent as DocIcon } from "../assets/icon-document.svg";
+import { ReactComponent as SaveIcon } from "../assets/icon-save.svg";
+import { ReactComponent as DeleteIcon } from "../assets/icon-delete.svg";
 
 export const Header = () => {
-  return <div className="">Header</div>;
+  return (
+    <div className="w-full h-72 bg-dark-gray-2 flex justify-between items-center pr-4">
+      <div className="h-full w-fit flex items-center gap-6">
+        <div className="h-full w-72 bg-dark-gray flex items-center justify-center cursor-pointer">
+          <img src={MenuIcon} alt="" />
+        </div>
+        <div className="h-full py-4 flex items-center gap-6">
+          <span>
+            <Logo />
+          </span>
+          <div className="w-fit h-full border-l border-light-gray-4 px-5-2 flex items-center gap-4">
+            <DocIcon />
+            <div>
+              <p className="text-light-gray-3 font-light text-sm">
+                Document Name
+              </p>
+              <p className="text-white font-normal text-base">welcome.md</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center gap-6">
+        <DeleteIcon className="cursor-pointer fill-light-gray-3 hover:fill-orange" />
+        <button className="btn">
+          <SaveIcon /> Save Changes
+        </button>
+      </div>
+    </div>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray": "#35393F",
+        "dark-gray-2": "#2B2D31",
+        "light-gray-3": "#7C8187",
+        "light-gray-4": "#5A6069",
+        orange: "#E46643",
+        "orange-light": "#F39765",
+      },
+      spacing: {
+        72: "72px",
+        "5-2": "24px",
+      },
+    },
   },
   plugins: [],
 };
